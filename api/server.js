b8f924721e14e1a6c249296a4f19aeda422da5e2
Leslie Thompson <lcrthompson@gmail.com>
2019-06-21 12:25:33 -0400

    server test added but has errors.debugging

--- api/server.js
+++ api/server.js
@@ -1,12 +1,29 @@
 const express = require("express");
-
 const server = express();
-
 server.use(express.json());
 
 //sanity check
-server.get("/", (req, res) => {
-  res.status(200).json({ message: "hey, girl!" });
-});
+server
+  .get("/", (req, res) => {
+    res.status(200).json({ message: "hey, girl!" });
+  })
+  .get("/games", async (req, res) => {
+    try {
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
+  })
+  .post("/games", async (req, res) => {
+    const game = req.body;
+    if (game.title && game.genre) {
+      try {
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    } else {
+      res.status(400).json({ message: "title and genre are required" });
+    }
+  });
 
 module.exports = server;

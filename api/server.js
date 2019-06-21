868cb3d5e6e735cdd59ddf41843ca1895378ddfb
Leslie Thompson <lcrthompson@gmail.com>
2019-06-21 13:52:26 -0400

    get tests passing

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const server = express();
 server.use(express.json());
+const db = require("../games/games-model.js");
 
 //sanity check
 server
@@ -9,13 +10,16 @@ server
   })
   .get("/games", async (req, res) => {
     try {
+      const games = await db.get("games");
+      res.status(200).json(games);
     } catch (error) {
       console.log(error);
       res.status(500).json(error);
     }
   })
   .post("/games", async (req, res) => {
-    const game = { ...req.body };
+    const game = req.body;
+
     if (!game.title && !game.genre) {
       return res.status(422).json({ message: "title and genre required" });
     }
